91b2fc3e6944fc540eac199134e81569922d79b2
kapoorkirpa07 <97598349+kapoorkirpa07@users.noreply.github.com>
2022-05-21 17:23:13 +0530

    Add files via upload

---
+++ parachute.js
@@ -0,0 +1,24 @@
+class Parachute {
+    constructor(x, y, width, height, paraPos) {
+    
+      this.body = Bodies.rectangle(x, y, width, height);
+      this.width = width;
+      this.height = height;
+  
+      this.image = loadImage("parachuteimg.png");
+      this.paraPosition = paraPos;
+      World.add(world, this.body);
+    }
+  
+    display() {
+      var angle = this.body.angle;
+      var pos = this.body.position;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(this.image, 0, 0, this.width, this.height);
+      pop();
+    }
+  }
